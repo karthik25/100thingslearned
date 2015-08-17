89297def72f08b5515885fa55fb6d22ae6702726
Karthik Ananthapadmanaban <karthika25@gmail.com>
2015-08-16 23:08:35 -0400

    using a template url instead of hard coded html

--- 100thingslearned-core.js
+++ 100thingslearned-core.js
@@ -297,7 +297,7 @@ mainModule.controller("mainController", function ($scope, $http, $timeout, $log)
 mainModule.directive("movie", function (){
     return {
       restrict: 'E',
-      template: "<div><a href=\"{{ entry.MessageBoardLink }}\" target=\"_blank\"><img ng-src=\"{{entry.ImageRelativeUrl}}\" alt=\"{{entry.Title}}\" /></a><h4 class=\"service-heading\"><a href=\"{{entry.MessageBoardLink}}\" target=\"_blank\">{{entry.Title}}</a></h4><p class=\"text-muted\">{{entry.Description}}</p></div>",
+      templateUrl: "movie.html",
       replace: true,
       scope: {
           entry: "="
